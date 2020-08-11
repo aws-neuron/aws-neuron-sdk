fbf8c316679e5f767d0853b5abd6f46a6acb7f67
Haichen "HC" Li <lhaiche@amazon.com>
2020-08-10 23:23:53 -0700

    [bert_no_model] support arbitrary sequence length
    Sequence length still defaults to 128. Also default batch size to 4.

--- src/examples/tensorflow/bert_demo/bert_no_model.py
+++ src/examples/tensorflow/bert_demo/bert_no_model.py
@@ -8,12 +8,18 @@ def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('--input_saved_model', required=True, help='Original SaveModel')
     parser.add_argument('--output_saved_model', required=True, help='Output SavedModel that runs on Inferentia')
-    parser.add_argument('--batch_size', type=int, default=1)
+    parser.add_argument('--batch_size', type=int, default=4)
+    parser.add_argument('--sequence_length', type=int, default=128)
     args = parser.parse_args()
-    compiler_args = [
-        '--enable-experimental-tensorized-scheduler',
-    ]
     pred = tf.contrib.predictor.from_saved_model(args.input_saved_model)
+    model_shape_feed_dict = {}
+    for key, value in pred.feed_tensors.items():
+        shape = value.shape.as_list()
+        if len(shape) > 0:
+            shape[0] = args.batch_size
+        if len(shape) > 1:
+            shape[1] = args.sequence_length
+        model_shape_feed_dict[key] = shape
     no_fuse_ops = [op.name for op in pred.graph.get_operations()]
 
     def force_fuse_condition(op_name):
@@ -32,10 +38,13 @@ def main():
     force_fuse_ops = [op.name for op in pred.graph.get_operations() if force_fuse_condition(op.name)]
     compilation_result = tfn.saved_model.compile(
         args.input_saved_model, args.output_saved_model,
-        batch_size=args.batch_size,
-        compiler_args=compiler_args,
+        model_shape_feed_dict=model_shape_feed_dict,
+        compiler_args=['-O2'],
+        compiler_timeout=36000,
         no_fuse_ops=no_fuse_ops,
         force_fuse_ops=force_fuse_ops,
+        minimum_segment_size=20,
+        dynamic_batch_size=True,
     )
     print(compilation_result)
 
