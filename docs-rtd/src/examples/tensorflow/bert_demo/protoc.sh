0019d33699613d745c2ad17907be9e6ef8e23616
Maen Suleiman <1706474+aws-maens@users.noreply.github.com>
2020-12-17 12:18:17 -0800

    Delete docs-rtd/src/ and add extension to make ../src available to rst files

--- docs-rtd/src/examples/tensorflow/bert_demo/protoc.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# coding=utf-8
-
-""" Copyright 2019 Amazon.com, Inc. or its affiliates. All Rights Reserved.
-    SPDX-License-Identifier: MIT-0
-    Program to gather information from a system
-"""
-
-python -m grpc_tools.protoc -I . --python_out=. --grpc_python_out=. mrpc.proto
