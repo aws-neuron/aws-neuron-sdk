0d876a0de4772550443c34446a392663a6e3644c
Maen Suleiman <1706474+aws-maens@users.noreply.github.com>
2021-11-01 15:35:10 -0700

    [prod] Add custom robots.txt and custom sitemap (merge into prod) (#408)
    * SOE related files

--- conf.py
+++ conf.py
@@ -56,7 +56,7 @@ templates_path = ['_templates']
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
 exclude_patterns = ['_build','**.ipynb_checkpoints']
-
+html_extra_path = ['static']
 
 # nbsphinx_allow_errors = True
 nbsphinx_execute = 'never'
