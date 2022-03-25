521d7744e4aa23db914e3f540c9bedfcf7836812
Amer Rez <81449018+aws-amerrez@users.noreply.github.com>
2022-03-24 21:57:02 -0700

    weights download fix (#468)
    * weights download fix
    
    * using the variable id instead of hardcoded url

--- src/examples/tensorflow/yolo_v4_demo/yolo_v4_coco_saved_model.py
+++ src/examples/tensorflow/yolo_v4_demo/yolo_v4_coco_saved_model.py
@@ -11,20 +11,9 @@ from tensorflow.keras import layers
 
 
 def download_file_from_google_drive(id):
-    URL = 'https://docs.google.com/uc?export=download'
-    session = requests.Session()
-    response = session.get(URL, params={'id': id}, stream=True)
-    token = get_confirm_token(response)
-    if token:
-        params = {'id': id, 'confirm': token}
-        response = session.get(URL, params=params, stream=True)
+    data = {'export': 'download', 'confirm': 't'}
+    response = requests.post('https://drive.google.com/uc?id='+id, data=data)
     return save_response_content(response)
-
-def get_confirm_token(response):
-    for key, value in response.cookies.items():
-        if key.startswith('download_warning'):
-            return value
-    return None
 
 def save_response_content(response):
     CHUNK_SIZE = 32768
